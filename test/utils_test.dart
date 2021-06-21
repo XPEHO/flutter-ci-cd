c3b09c437aee8458f1fa58842f9af287b69323e7
NayMak <makusa.nayden@gmail.com>
2021-06-21 09:28:38 +0200

    feat(test-ci): sum function replaced by multiply, tests updated consequently + test.yml deleted (#4)

--- test/utils_test.dart
+++ test/utils_test.dart
@@ -2,9 +2,17 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fluttercicd/utils.dart';
 
 void main() {
-  test('sum function testing', () {
-    final result = sum(36, 6);
+  group('Multiply function testing', () {
+    test('Simple multiplication', () {
+      final result = multiply(6, 7);
 
-    expect(result, 42);
+      expect(result, 42, reason: 'Regular multiplication should work');
+    });
+
+    test('Ensure correct handling of zero', () {
+      final result = multiply(13, 0);
+
+      expect(result, 0, reason: 'Multiple with zero should be zero');
+    });
   });
 }
